8ccfb6122b34ced4b796c6309c9c9116396a18d5
karce <karce@ucm.es>
2023-11-01 16:04:52 +0100

    Fallo estructura

--- reverie/backend_server/utils.py
+++ reverie/backend_server/utils.py
@@ -1,8 +1,6 @@
 # Copy and paste your OpenAI API Key
 import os
 api_key = os.environ['api_key']
-# Put your name
-key_owner = os.environ['key_owner']
 
 maze_assets_loc = "../../environment/frontend_server/static_dirs/assets"
 env_matrix = f"{maze_assets_loc}/the_ville/matrix"
