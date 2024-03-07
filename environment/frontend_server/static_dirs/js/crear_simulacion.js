106c93a3453173fcdd3c47dd62dbcaf77583c187
Alberto Ramos Suárez <bertoramos2001@gmail.com>
2024-03-07 20:49:07 +0100

    Nueva vista crear simulacion

--- environment/frontend_server/static_dirs/js/crear_simulacion.js
+++ environment/frontend_server/static_dirs/js/crear_simulacion.js
@@ -17,14 +17,41 @@ $(document).ready(function() {
   
       for (let i = 1; i <= numPersonajes; i++) { 
         $('#contenedorPersonaje').append(`
-          <div class="character-form">
-            <h3>Personaje ${i}</h3>
+          <div class="container character-form">
+            <h3 class="fw-bold">Personaje ${i}</h3>
             <select name="nombre${i}" id="nombre${i}" required>
                 <option value="" disabled selected>Selecciona un nombre</option>
               ${nombres.map(nombre => `<option value="${nombre}">${nombre}</option>`)}
             </select>
-            <label for="contexto${i}">Personalidad:</label>
-            <textarea name="contexto${i}" id="contexto${i}" required></textarea>
+            <div class="row">
+              <div class="col-md-6">
+                <label for="innate${i}" class="fw-bold">Personalidad innata:</label>
+                <select multiple name="innate${i}[]" id="innate${i}" class="mb-0" required> 
+                    <option value="friendly">Amigable</option>
+                    <option value="outgoing">Extrovertido</option>
+                    <option value="hospitable">Hospitalario</option>
+                    <option value="passionate">Apasionado</option>
+                    <option value="kind">Amable</option>
+                    <option value="energetic">Energético</option>
+                    <option value="enthusiastic">Entusiasta</option>
+                </select>
+                <small class="text-muted">Selecciona con Ctrl o Cmd hasta un máximo de 3 personalidades</small>
+              </div>
+              <div class="col-md-6">
+                <label for="lifestyle${i}" class="fw-bold">Estilo de vida:</label>
+                <textarea name="lifestyle${i}" id="lifestyle${i}" placeholder="Breve texto sobre el estilo de vida del agente" required></textarea>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <label for="learned${i}" class="fw-bold">Conocimiento aprendido:</label>
+                <textarea name="learned${i}" id="learned${i}" placeholder="Breve texto sobre los recuerdos iniciales del personaje" required></textarea>
+              </div>
+              <div class="col-md-6">
+                <label for="currently${i}" class="fw-bold">Estado actual:</label>
+                <textarea name="currently${i}" id="currently${i}" placeholder="Breve texto sobre las pretensiones e inquietudes actuales" required></textarea>
+              </div>
+            </div>
           </div>
         `);
       }
