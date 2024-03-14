100f25dcde98924f67726f821f1330c60bd2d88c
karce <karce@ucm.es>
2024-03-14 22:17:43 +0100

    Funcionalidades run, save, finish, exit añadidas al endpoint

--- environment/frontend_server/endpoint/ReverieComm.py
+++ environment/frontend_server/endpoint/ReverieComm.py
@@ -9,6 +9,7 @@ sys.path.append(backend_dir)
 
 # Este será un fifo
 INPUT_ENDPOINT = f"{local_dir}/reverieInput"
+
 # Este es un regular file
 # Preferiría hacerlo fifo, pero si no hay nadie leyendo se bloquea
 OUTPUT_ENDPOINT = f"{local_dir}/reverieOutput"
@@ -16,8 +17,77 @@ OUTPUT_ENDPOINT = f"{local_dir}/reverieOutput"
 # Aquí se enviarán los errores
 ERR_ENDPOINT = f"{local_dir}/reverieError"
 
+PID_INFO_FILE = f"{local_dir}/reverie_pid"
+
 from reverie import ReverieServer
 
 class ReverieComm(ReverieServer):
-  def __init__():
-    ReverieServer.__ini__()
+  def redirect_std(self):
+    if not os.path.exists(ReverieComm.INPUT_ENDPOINT):
+      os.mkfifo(INPUT_ENDPOINT)
+
+    fdIn = os.open(INPUT_ENDPOINT, os.O_RDONLY | os.O_CREAT)
+    os.dup2(fdIn, 0)
+    os.close(fdIn)
+    
+    fdOut = os.open(OUTPUT_ENDPOINT, os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
+    os.dup2(fdOut, 1)
+    os.close(fdOut)
+    
+    fdErr = os.open(ERR_ENDPOINT, os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
+    os.dup2(fdErr, 2)
+    os.close(fdErr)
+
+  def __init__(self,
+               forked,
+               params):
+    # Redirigimos el input output que usará el reverie server a los pipes
+    self.redirect_std()
+
+    # Creamos el Reverie Server == Los archivos de la nueva simulación
+    ReverieServer.__ini__(forked, params)
+
+    # Guardamos el pid en un fichero para terminar el proceso cuando se termine con la simulacion desde el front
+    with open(PID_INFO_FILE, 'w') as pid_file:
+      pid_file.write(os.getpid())
+    
+    # Si ocurre algun problema desde el back se aborta la ejecución de la simulación y escribimos los fallos en la salida de error
+    try:
+      self.open_server()
+    except Exception as e:
+      print(e.with_traceback, file=sys.stderr)
+
+  @staticmethod
+  def write_command(command):
+    with open(INPUT_ENDPOINT, 'w') as in_file:
+      in_file.write(command)
+
+  @staticmethod
+  def run(n_steps):
+    """
+    Ejecuta el numero de pasos especificado
+    """
+    ReverieComm.write_command(f"run {n_steps}")
+
+  @staticmethod
+  def save():
+    """
+    Guarda el progreso
+    """
+    ReverieComm.write_command("save")
+
+  @staticmethod
+  def finish():
+    """
+    Termina la simulación y guarda el progreso
+    """
+    ReverieComm.write_command("finish")
+
+  @staticmethod
+  def exit():
+    """
+    Termina la simulación
+    No guarda el estado
+    Y elimina toda la información de la simulación
+    """
+    ReverieComm.write_command("exit")
--- environment/frontend_server/endpoint/prueba.py
+++ environment/frontend_server/endpoint/prueba.py
@@ -8,35 +8,37 @@ if __name__ == '__main__':
     if pid == 0:
         if not os.path.exists(ReverieComm.INPUT_ENDPOINT):
             os.mkfifo(ReverieComm.INPUT_ENDPOINT)
-        with open(ReverieComm.OUTPUT_ENDPOINT, 'w') as out_f:
-            with open(ReverieComm.ERR_ENDPOINT, 'w') as out_err:
-                fdIn = os.open(ReverieComm.INPUT_ENDPOINT, os.O_RDONLY | os.O_CREAT)
-                fdOut = out_f.fileno()
-                fdErr = out_err.fileno()
-                
-                os.dup2(fdIn, 0)
-                os.dup2(fdOut, 1)
-                os.dup2(fdErr, 2)
-                i = 0
-                while True:
-                    try:
-                        variable = sys.stdin.readline()
-                        if not variable:
-                            pass
-                        if (len(variable) == 0):
-                            time.sleep(1)
-                        else:
-                            i += 1
-                            print(f"{i}:")
-                            print("Esto es lo que me has dado: ", variable)
-                    except Exception as e:
-                        """
-                        Si ocurre algún error inesperado habría que eliminar el proceso del ReverieServer desde una excepción como esta
-                        Quizá la excepción se haría en la reescritura de start_server, donde se esperan los comandos
-                        """
-                        print(e.with_traceback, file=sys.stderr)
-                        
 
-                os.close(fdIn)
+        fdIn = os.open(ReverieComm.INPUT_ENDPOINT, os.O_RDONLY | os.O_CREAT)
+        os.dup2(fdIn, 0)
+        os.close(fdIn)
+        
+        fdOut = os.open(ReverieComm.OUTPUT_ENDPOINT, os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
+        os.dup2(fdOut, 1)
+        os.close(fdOut)
+        
+        fdErr = os.open(ReverieComm.ERR_ENDPOINT, os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
+        os.dup2(fdErr, 2)
+        os.close(fdErr)
+
+        i = 0
+        while True:
+            try:
+                variable = sys.stdin.readline()
+                if not variable:
+                    pass
+                if (len(variable) == 0):
+                    time.sleep(1)
+                else:
+                    i += 1
+                    print(f"{i}:")
+                    print("Esto es lo que me has dado: ", variable)
+            except Exception as e:
+                """
+                Si ocurre algún error inesperado habría que eliminar el proceso del ReverieServer desde una excepción como esta
+                Quizá la excepción se haría en la reescritura de start_server, donde se esperan los comandos
+                """
+                print(e.with_traceback, file=sys.stderr)
+                        
     else:
         print(pid)
