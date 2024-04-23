e9b2fbb80e3739a90bff1c9b268a1e67e5e10bb0
Alberto Ramos Suárez <bertoramos2001@gmail.com>
2024-04-23 20:30:09 +0200

    pantalla home simulator hecha front

--- environment/frontend_server/static_dirs/js/llamadas_home.js
+++ environment/frontend_server/static_dirs/js/llamadas_home.js
@@ -2,37 +2,82 @@
 // (play, pase, guardar para ver, guardar para continuar y salir sin guardar)
 
 $(document).ready(function() {
-    $('#boton_play').click(function() {
-        // get the value from the input number field
+    let simulacionCorriendo = true;
+
+    const simulationStateText = $('#textoEstadoSimulacion');
+    const boton_play = $('#play_button');
+    const boton_pause = $('#pause_button');
+    const boton_run = $('#boton_run');
+    const input_steps = $('#num_steps');    
+    const boton_guardar_ver = $('#boton_guardar_ver');
+    const boton_guardar_y_salir = $('#boton_guardar_y_salir');
+    const boton_salir = $('#boton_salir');
+    const boton_chat = $('.boton_chat');
+    const boton_susurro = $('.boton_susurro');
+    const boton_abrir_chat = $('.abrir_chat');
+    const boton_abrir_susurro = $('.abrir_susurro');
+
+    function gestionarVisibilidad() {
+        boton_run.prop('disabled', !simulacionCorriendo);
+        input_steps.prop('disabled', !simulacionCorriendo);
+        boton_guardar_ver.prop('disabled', simulacionCorriendo);
+        boton_guardar_y_salir.prop('disabled', simulacionCorriendo);
+        boton_salir.prop('disabled', simulacionCorriendo);
+        boton_abrir_chat.prop('disabled', simulacionCorriendo);
+        boton_abrir_susurro.prop('disabled', simulacionCorriendo);
+    }
+
+    function actualizarTextoSimulacion() {
+        const message = simulacionCorriendo ? "La simulación se está ejecutando..." : "La simulación está parada";
+        const color = simulacionCorriendo ? 'green' : 'red';
+        simulationStateText.text(message).css('color', color);
+    }
+
+    gestionarVisibilidad();
+    actualizarTextoSimulacion();
+
+    boton_play.click(function() {
+        simulacionCorriendo = true; // Estado de la simulación corriendo (se realizan llamadas a process environment)
+        gestionarVisibilidad();
+        actualizarTextoSimulacion();
+        console.log("Iniciando la simulación " + simulacionCorriendo)
+    });
+
+    boton_pause.click(function() {
+        simulacionCorriendo = false; // Estado de la simulación pausada (no se realizan llamadas a process environment)
+        gestionarVisibilidad();
+        actualizarTextoSimulacion();
+
+        console.log("Pausando la simulación " + simulacionCorriendo)
+        // sendAjaxCall('pause');
+    });
+
+    boton_run.click(function() {
+        // Obtener el valor del select de pasos
         let values = {};
         let steps = $('#step-select').val();
         values['steps'] = steps;
-        console.log(steps)
-        console.log("Iniciando la simulación")
-        sendAjaxCall('play', values);
+
+        console.log(steps);
+        sendAjaxCall('run', values);
     });
 
-    $('#pause_button').click(function() {
-        console.log("Pausando la simulación")
-        sendAjaxCall('pause');
-    });
-
-    $('#boton_guardar_ver').click(function() {
+    boton_guardar_ver.click(function() {
         console.log("Guardando la simulación para ver después")
         sendAjaxCall('guardar_ver');
     });
     
-    $('#boton_guardar_y_salir').click(function() {
+    boton_guardar_y_salir.click(function() {
         console.log("Guardando la simulación para continuar después")
         sendAjaxCall('guardar_salir');
     });
 
-    $('#boton_salir').click(function() {
+    boton_salir.click(function() {
         console.log("Saliendo de la simulación")
         sendAjaxCall('salir');
     });
 
-    $('.boton_chat').click(function() {
+    boton_chat.click(function() {
         const chat = $(this).closest('.modal').find('textarea').val(); 
         const personaName = $(this).closest('.modal').find('input').val();
 
@@ -46,7 +91,7 @@ $(document).ready(function() {
         sendAjaxCall('chat', values);
     });
 
-    $('.boton_susurro').click(function() {
+    boton_susurro.click(function() {
         const susurro = $(this).closest('.modal').find('textarea').val(); 
         const personaName = $(this).closest('.modal').find('input').val();
 
