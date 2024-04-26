df1ccbadd3a5fa7ee18f63e81ada289826f77d2d
karce <karce@ucm.es>
2024-04-26 12:02:58 +0200

    Unificados continuar y fork

--- reverie/backend_server/reverie.py
+++ reverie/backend_server/reverie.py
@@ -240,6 +240,35 @@ class ReverieServer:
     self.load_meta(sim_folder)
     self.signal_front_end()
 
+  def fork_sim(self, 
+               fork_sim_code,
+               sim_code):
+
+    # FORKING FROM A PRIOR SIMULATION:
+    # <fork_sim_code> indicates the simulation we are forking from. 
+    # Interestingly, all simulations must be forked from some initial 
+    # simulation, where the first simulation is "hand-crafted".
+    self.fork_sim_code = fork_sim_code
+    fork_folder = f"{fs_storage}/{self.fork_sim_code}"
+
+    # <sim_code> indicates our current simulation. The first step here is to 
+    # copy everything that's in <fork_sim_code>, but edit its 
+    # reverie/meta/json's fork variable. 
+    self.sim_code = sim_code
+    sim_folder = f"{fs_storage}/{self.sim_code}"
+    copyanything(fork_folder, sim_folder)
+
+    with open(f"{sim_folder}/reverie/meta.json") as json_file:  
+      reverie_meta = json.load(json_file)
+
+    #En el archivo de la nueva simulación estoy seteando el <dato fork_sim_code>
+    with open(f"{sim_folder}/reverie/meta.json", "w") as outfile: 
+      reverie_meta["fork_sim_code"] = fork_sim_code
+      reverie_meta["sim_code"] = sim_code
+      outfile.write(json.dumps(reverie_meta, indent=2))
+    self.load_meta(sim_folder)
+    self.signal_front_end()
+
   def load_meta(self, sim_folder):
     with open(f"{sim_folder}/reverie/meta.json") as json_file:  
       reverie_meta = json.load(json_file)
@@ -315,110 +344,6 @@ class ReverieServer:
     # <server_sleep> denotes the amount of time that our while loop rests each
     # cycle; this is to not kill our machine. 
     self.server_sleep = 0.1
-
-
-
-
-  def fork_sim(self, 
-               fork_sim_code,
-               sim_code):
-
-    # FORKING FROM A PRIOR SIMULATION:
-    # <fork_sim_code> indicates the simulation we are forking from. 
-    # Interestingly, all simulations must be forked from some initial 
-    # simulation, where the first simulation is "hand-crafted".
-    self.fork_sim_code = fork_sim_code
-    fork_folder = f"{fs_storage}/{self.fork_sim_code}"
-
-    # <sim_code> indicates our current simulation. The first step here is to 
-    # copy everything that's in <fork_sim_code>, but edit its 
-    # reverie/meta/json's fork variable. 
-    self.sim_code = sim_code
-    sim_folder = f"{fs_storage}/{self.sim_code}"
-    copyanything(fork_folder, sim_folder)
-
-    with open(f"{sim_folder}/reverie/meta.json") as json_file:  
-      reverie_meta = json.load(json_file)
-
-    #En el archivo de la nueva simulación estoy seteando el <dato fork_sim_code>
-    with open(f"{sim_folder}/reverie/meta.json", "w") as outfile: 
-      reverie_meta["fork_sim_code"] = fork_sim_code
-      reverie_meta["sim_code"] = sim_code
-      outfile.write(json.dumps(reverie_meta, indent=2))
-
-    # LOADING REVERIE'S GLOBAL VARIABLES
-    # The start datetime of the Reverie: 
-    # <start_datetime> is the datetime instance for the start datetime of 
-    # the Reverie instance. Once it is set, this is not really meant to 
-    # change. It takes a string date in the following example form: 
-    # "June 25, 2022"
-    # e.g., ...strptime(June 25, 2022, "%B %d, %Y")
-    self.start_time = datetime.datetime.strptime(
-                        f"{reverie_meta['start_date']}, 00:00:00",  
-                        "%B %d, %Y, %H:%M:%S")
-    # <curr_time> is the datetime instance that indicates the game's current
-    # time. This gets incremented by <sec_per_step> amount everytime the world
-    # progresses (that is, everytime curr_env_file is recieved). 
-    self.curr_time = datetime.datetime.strptime(reverie_meta['curr_time'], 
-                                                "%B %d, %Y, %H:%M:%S")
-    # <sec_per_step> denotes the number of seconds in game time that each 
-    # step moves foward. 
-    self.sec_per_step = reverie_meta['sec_per_step']
-    
-    # <maze> is the main Maze instance. Note that we pass in the maze_name
-    # (e.g., "double_studio") to instantiate Maze. 
-    # e.g., Maze("double_studio")
-    self.maze = Maze(reverie_meta['maze_name'])
-    
-    # <step> denotes the number of steps that our game has taken. A step here
-    # literally translates to the number of moves our personas made in terms
-    # of the number of tiles. 
-    self.step = reverie_meta['step']
-
-    # SETTING UP PERSONAS IN REVERIE
-    # <personas> is a dictionary that takes the persona's full name as its 
-    # keys, and the actual persona instance as its values.
-    # This dictionary is meant to keep track of all personas who are part of
-    # the Reverie instance. 
-    # e.g., ["Isabella Rodriguez"] = Persona("Isabella Rodriguezs")
-    self.personas = dict()
-    # <personas_tile> is a dictionary that contains the tile location of
-    # the personas (!-> NOT px tile, but the actual tile coordinate).
-    # The tile take the form of a set, (row, col). 
-    # e.g., ["Isabella Rodriguez"] = (58, 39)
-    self.personas_tile = dict()
-    
-    # # <persona_convo_match> is a dictionary that describes which of the two
-    # # personas are talking to each other. It takes a key of a persona's full
-    # # name, and value of another persona's full name who is talking to the 
-    # # original persona. 
-    # # e.g., dict["Isabella Rodriguez"] = ["Maria Lopez"]
-    # self.persona_convo_match = dict()
-    # # <persona_convo> contains the actual content of the conversations. It
-    # # takes as keys, a pair of persona names, and val of a string convo. 
-    # # Note that the key pairs are *ordered alphabetically*. 
-    # # e.g., dict[("Adam Abraham", "Zane Xu")] = "Adam: baba \n Zane:..."
-    # self.persona_convo = dict()
-
-    # Loading in all personas. 
-    init_env_file = f"{sim_folder}/environment/{str(self.step)}.json"
-    init_env = json.load(open(init_env_file))
-    for persona_name in reverie_meta['persona_names']: 
-      persona_folder = f"{sim_folder}/personas/{persona_name}"
-      p_x = init_env[persona_name]["x"]
-      p_y = init_env[persona_name]["y"]
-      curr_persona = Persona(persona_name, persona_folder)
-
-      self.personas[persona_name] = curr_persona
-      self.personas_tile[persona_name] = (p_x, p_y)
-      self.maze.tiles[p_y][p_x]["events"].add(curr_persona.scratch
-                                              .get_curr_event_and_desc())
-
-    # REVERIE SETTINGS PARAMETERS:  
-    # <server_sleep> denotes the amount of time that our while loop rests each
-    # cycle; this is to not kill our machine. 
-    self.server_sleep = 0.1
-    self.signal_front_end()
 
   def signal_front_end(self):
     # SIGNALING THE FRONTEND SERVER: 
